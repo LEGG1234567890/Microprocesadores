bc8f07e92e48998b29d092e9eee6bfa0243c3b3f
LEGG1234567890 <141190211+LEGG1234567890@users.noreply.github.com>
2026-06-15 16:18:25 -0600

    commit

--- Proyecto1/Proyecto1.X/main.c
+++ Proyecto1/Proyecto1.X/main.c
@@ -24,9 +24,37 @@
 
 #define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
 
-unsigned char lado=1;
+unsigned char lado=0, tiempo=0;   //200 - 400
 
-const unsigned char carita[8] = {
+void ADC_Init(){
+    ANSEL = 0x03;
+    ANSELH = 0;
+    ADCON0 = 0x01;
+    ADCON1 = 0x81;
+}
+
+unsigned int ADC_Read(unsigned char channel){
+    __delay_us(5);
+    ADCON0 &= 0x83;
+    ADCON0 |= channel<<2;
+    __delay_ms(2);
+    GO_nDONE = 1;
+    while(GO_nDONE);
+    return ((ADRESH<<8)+ADRESL);
+}
+
+const unsigned char triste[8] = {
+    0b00000,
+    0b01010,
+    0b01010,
+    0b00000,
+    0b01110,
+    0b10001,
+    0b00000,
+    0b00000
+};
+
+const unsigned char feliz[8] = {
     0b00000,
     0b01010,
     0b01010,
@@ -37,32 +65,20 @@ const unsigned char carita[8] = {
     0b00000
 };
 
-const unsigned char casita[8] = {
-    0b00100,
-    0b01110,
-    0b11111,
-    0b10001,
-    0b10001,
-    0b10001,
-    0b11111,
-    0b00000
-};
-
-void LCD_CreateChar(unsigned char pos, const unsigned char *pattern)
-{
+void LCD_CreateChar(unsigned char pos, const unsigned char *pattern){
     LCD_Cmd(0x40 + (pos * 8));   // Ir a la CGRAM
-
-    for(unsigned char i = 0; i < 8; i++)
-    {
+    for(unsigned char i = 0; i < 8; i++){
         LCD_putc(pattern[i]);    // Copiar los 8 bytes
     }
-
     LCD_Cmd(0x80);               // Regresar a memoria normal
 }
 
 void main(void){
-    ANSEL  = 0;
-    ANSELH = 0;
+    ADC_Init();
+    LCD lcd = {&PORTC, 2, 3, 4, 5, 6, 7};
+    LCD_Init(lcd);
+    LCD_CreateChar(0, triste);
+    LCD_CreateChar(1, feliz);
     TRISD = 0b00000000;
     TRISC = 0;
     TRISB = 0x01;
@@ -73,24 +89,41 @@ void main(void){
     INTF = 0;
     INTE = 1;
     GIE = 1;
-    LCD lcd = {&PORTC, 2, 3, 4, 5, 6, 7};
-    LCD_Init(lcd);
-    LCD_CreateChar(0, carita);
-    LCD_CreateChar(1, casita);
+    int x=0, y=0;
     while(1){
         LCD_Clear();
-        LCD_Set_Cursor(0, 0);
-        if(lado){
-            LCD_putrs("Hola mundo!");
-            LCD_Set_Cursor(1, 0);
-            LCD_putc(0);     // Mostrar la carita
-            __delay_ms(300);
-        }else{
-            LCD_putrs("Adios mundo!");
-            LCD_Set_Cursor(1, 0);
-            LCD_putc(1);     // Mostrar la casita
+        unsigned int adc_x = ADC_Read(0), adc_y = ADC_Read(1);
+        if(adc_y<500){
+            y=0;
+        }else if(adc_y>524){
+            y=1;
         }
-        __delay_ms(1000);
+        if(adc_x<256){
+            x--;
+            tiempo=1;
+        }else if((255<adc_x)&&(adc_x<500)){
+            x--;
+            tiempo=0;
+        }else if((524<adc_x)&&(adc_x<768)){
+            x++;
+            tiempo=0;
+        }else if(adc_x>767){
+            x++;
+            tiempo=1;
+        }
+        if(x > 15) x = 0;
+        if(x < 0) x = 15;
+        LCD_Set_Cursor(y, x);
+        if(lado){
+            LCD_putc(0);
+        }else{
+            LCD_putc(1);
+        }
+        if(tiempo){
+            __delay_ms(250);
+        }else{
+            __delay_ms(500);
+        }
     }
 }
 
