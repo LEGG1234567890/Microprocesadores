87feb1334bc283efd2578173f9a769eeca6026c3
LEGG1234567890 <141190211+LEGG1234567890@users.noreply.github.com>
2026-06-24 15:24:34 -0600

    commit

--- Practica12/Practica12.X/main.c
+++ Practica12/Practica12.X/main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "lcd.h"
 
 #pragma config FOSC = HS
@@ -56,7 +57,9 @@ char Keypad_Scan(void) {
         for (col = 0; col < 4; col++) {
             if (!(lectura & (0x10 << col))) {
                 __delay_ms(20);
-                do { lectura = PORTB; } while (!(lectura & (0x10 << col)));
+                do {
+                    lectura = PORTB;
+                } while (!(lectura & (0x10 << col)));
                 return keymap[fila][col];
             }
         }
@@ -73,30 +76,48 @@ void mostrar_resultado(long resultado) {
     LCD_puts(res);
 }
 
+void mostrar_division(long num1, long num2) {
+    char res[17];
+    long entero = num1 / num2;
+    long residuo = num1 % num2;
+    LCD_Clear();
+    __delay_ms(2);
+    LCD_Set_Cursor(0, 0);
+    if (residuo == 0) {
+        long_a_str(entero, res);
+    } else {
+        unsigned long decimales =
+            ((unsigned long)residuo * 1000UL) / num2;
+        sprintf(res, "%ld.%03lu", entero, decimales);
+    }
+    LCD_puts(res);
+}
+
 void main(void) {
-    ANSEL  = 0;
+    ANSEL = 0;
     ANSELH = 0;
-    TRISB  = 0xF0;
-    PORTB  = 0x0F;
+    TRISB = 0xF0;
+    PORTB = 0x0F;
     OPTION_REG &= 0b01111111;
     TRISC = 0;
     PORTC = 0;
     LCD lcd = {&PORTC, 2, 3, 4, 5, 6, 7};
     LCD_Init(lcd);
     LCD_Set_Cursor(0, 0);
-    char buffer[9] = {0};
-    uint8_t pos     = 0;
-    long    num1    = 0;
-    long    num2    = 0;
-    char    operador = 0;
-    char    key;
+    char buffer[17] = {0};
+    uint8_t pos = 0;
+    long num1 = 0;
+    long num2 = 0;
+    char operador = 0;
+    char key;
     uint8_t esperando_num2 = 0;
     while (1) {
         key = Keypad_Scan();
-        if (key == 0) continue;
+        if (key == 0)
+            continue;
         if (key == 'C') {
-            // Borrar todo y reiniciar
-            num1 = 0; num2 = 0;
+            num1 = 0;
+            num2 = 0;
             operador = 0;
             esperando_num2 = 0;
             pos = 0;
@@ -104,47 +125,73 @@ void main(void) {
             LCD_Clear();
             __delay_ms(2);
             LCD_Set_Cursor(0, 0);
-        } else if (key == '+' || key == '-' || key == '*' || key == '/') {
-            num1 = atol(buffer);
-            operador = key;
-            esperando_num2 = 1;
-            pos = 0;
-            buffer[0] = '\0';
-            // Mostrar operador en LCD
-            LCD_putc(key);
-        } else if (key == '=') {
+        }else if (key == '+' || key == '-' ||
+                 key == '*' || key == '/') {
+            if (pos > 0) {
+                num1 = atol(buffer);
+                operador = key;
+                esperando_num2 = 1;
+                pos = 0;
+                buffer[0] = '\0';
+                LCD_putc(key);
+            }
+        }else if (key == '=') {
             if (operador != 0 && pos > 0) {
                 num2 = atol(buffer);
-                long resultado = 0;
-                if (operador == '+') resultado = num1 + num2;
-                if (operador == '-') resultado = num1 - num2;
-                if (operador == '*') resultado = num1 * num2;
-                if (operador == '/') {
-                    if (num2 != 0)
-                        resultado = num1 / num2;
-                    else {
+                if(operador == '/') {
+                    if (num2 == 0) {
                         LCD_Clear();
                         __delay_ms(2);
                         LCD_Set_Cursor(0, 0);
                         LCD_puts("Error /0");
-                        // Reiniciar
-                        num1=0; num2=0; operador=0;
-                        esperando_num2=0;
-                        pos=0; buffer[0]='\0';
+                        num1 = 0;
+                        num2 = 0;
+                        operador = 0;
+                        esperando_num2 = 0;
+                        pos = 0;
+                        buffer[0] = '\0';
                         continue;
                     }
+                    mostrar_division(num1, num2);
+                    long entero = num1 / num2;
+                    long residuo = num1 % num2;
+                    if (residuo == 0) {
+                        long_a_str(entero, buffer);
+                    } else {
+                        unsigned long decimales =
+                            ((unsigned long)residuo * 1000UL) / num2;
+                        sprintf(buffer,
+                                "%ld.%03lu",
+                                entero,
+                                decimales);
+                    }
+                    pos = 0;
+                    while (buffer[pos] != '\0')
+                        pos++;
+                    operador = 0;
+                    esperando_num2 = 0;
+                    continue;
+                }else {
+                    long resultado = 0;
+                    if (operador == '+')
+                        resultado = num1 + num2;
+                    if (operador == '-')
+                        resultado = num1 - num2;
+                    if (operador == '*')
+                        resultado = num1 * num2;
+                    mostrar_resultado(resultado);
+                    long_a_str(resultado, buffer);
+                    pos = 0;
+                    while (buffer[pos] != '\0')
+                        pos++;
+                    operador = 0;
+                    esperando_num2 = 0;
                 }
-                mostrar_resultado(resultado);
-                long_a_str(resultado, buffer);
-                pos = 0;
-                while (buffer[pos] != '\0') pos++;
-                operador = 0;
-                esperando_num2 = 0;
             }
-        } else {
-            if (pos < 8) {
+        }else {
+            if (pos < 16) {
                 buffer[pos++] = key;
-                buffer[pos]   = '\0';
+                buffer[pos] = '\0';
                 LCD_putc(key);
             }
         }
